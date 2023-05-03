40829af32fbe844706a7cb12d5d6829ffc39f064
Nickita0000 <nzadubniak@gmail.com>
2023-05-03 17:16:59 +0100

    Regexp

---
+++ index.js
@@ -0,0 +1,7 @@
+let regexp = /love/;
+console.log( regexp.test('I love JavaScript') ); // true
+console.log( regexp.test('I JavaScript') ); // false
+
+regexp = /^.*ing$/;
+console.log( regexp.test('Good morning') ); // true
+console.log( regexp.test('Good morning!') ); // false
